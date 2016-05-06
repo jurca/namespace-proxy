7bd27a00c8c55874c194e5ed442b8025a79ca686
Martin Jurča <mjurca@centrum.cz>
2016-05-06 19:44:41 +0200

    set up the testing environment

---
+++ test/createNamespaceProxySpec.js
@@ -0,0 +1,9 @@
+
+import createNamespaceProxy from "../es2015/createNamespaceProxy"
+import test from "tape"
+
+test("createNamespaceProxy", (t) => {
+  t.plan(0)
+  // TODO: write some tests
+  t.end()
+})
