09f0b6bda7c9bd952bf2e938070c5eb3ebd8c349
Martin Jurča <mjurca@centrum.cz>
2016-05-21 10:17:42 +0200

    defined the tests, implemented the first test

--- test/createNamespaceProxySpec.js
+++ test/createNamespaceProxySpec.js
@@ -2,8 +2,57 @@
 import createNamespaceProxy from "../es2015/createNamespaceProxy"
 import test from "tape"
 
-test("createNamespaceProxy", (t) => {
+test("createNamespaceProxy should store data in a namespace", (t) => {
+  t.plan(2)
+
+  let proxy = createNamespaceProxy()
+  let object = {
+    foo: "bar"
+  }
+  proxy(object).foo = "baz"
+
+  t.equal(object.foo, "bar")
+  t.equal(proxy(object).foo, "baz")
+
+  t.end()
+})
+
+test("createNamespaceProxy should use separate namespaces for each proxy",
+    (t) => {
   t.plan(0)
-  // TODO: write some tests
+  t.end()
+})
+
+test("createNamespaceProxy should use well-named symbols to store data",
+    (t) => {
+  t.plan(0)
+  t.end()
+})
+
+test("createNamespaceProxy should create non-enumerable properties by default",
+    (t) => {
+  t.plan(0)
+  t.end()
+})
+
+test("createNamespaceProxy should allow creation of enumerable properties",
+    (t) => {
+  t.plan(0)
+  t.end()
+})
+
+test("createNamespaceProxy should provide static symbols", (t) => {
+  t.plan(0)
+  t.end()
+})
+
+test("createNamespaceProxy should cache instance proxies", (t) => {
+  t.plan(0)
+  t.end()
+})
+
+test("createNamespaceProxy should use the same symbols across objects",
+    (t) => {
+  t.plan(0)
   t.end()
 })
